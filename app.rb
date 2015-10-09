680a72dfd29b7aa76af411cc9f994cace85d750a
Justin Bean <beanjustinr@gmail.com>
2015-10-09 13:25:51 -0400

    added to css and app.rb

--- app.rb
+++ app.rb
@@ -1,3 +1,15 @@
 require 'sinatra'
 require 'tilt/erb'
 require 'pry'
+
+location = []
+
+get '/' do
+  erb :index, locals: { position: nil }
+end
+
+post '/location' do
+  location = []
+  location.push(params[:position])
+  erb :index, locals: { position: location }
+end
